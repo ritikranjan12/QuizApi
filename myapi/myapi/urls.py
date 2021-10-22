7585f5456ab70ba8529e8505117fe28767e60d20
Ritik Ranjan <75849689+ritikranjan12@users.noreply.github.com>
2021-10-23 02:29:33 +0530

    new update

--- myapi/myapi/urls.py
+++ myapi/myapi/urls.py
@@ -19,5 +19,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('questionapi/', include('questionapi.urls')),
+    path('', include('questionapi.urls')),
 ]

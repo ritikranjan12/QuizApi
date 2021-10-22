6ad0595920525dd4a3b3bc00c36f439717e17f2c
Ritik Ranjan <75849689+ritikranjan12@users.noreply.github.com>
2021-10-23 01:56:14 +0530

    new update

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+django_heroku.settings(locals())
